3e3046a378aaab1c32e3723e028dbe3b6fd5f878
yeftanma <77386483+yeftanma@users.noreply.github.com>
2021-02-27 23:21:40 +0700

    Update 4

---
+++ BillSplitter3/CateringApp/LoginVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CateringApp
+{
+    class LoginVM
+    {
+        public CommandHandler BackBtn { get; private set; }
+        public CommandHandler LoginBtn { get; private set; }
+        public CommandHandler IssueBtn { get; private set; }
+        
+        public LoginVM()
+        {
+            BackBtn = new CommandHandler(back);
+        }
+
+        public void back()
+        {
+            MainWindow.Frame.Content = new SignUpPage();
+        }
+    }
+}
