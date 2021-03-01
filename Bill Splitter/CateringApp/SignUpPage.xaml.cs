ebf39eb18fe58772ccaa595d2d3dd9c3cd07553e
yeftanma <77386483+yeftanma@users.noreply.github.com>
2021-03-01 21:55:24 +0700

    Update Bill Splitter-01
    Edit Translation on hyperlink 'Log In'
    Change 'IssuePage' to 'ProblemPage'
    Change Text Alignment in RichTextBox

---
+++ Bill Splitter/CateringApp/SignUpPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CateringApp
+{
+    /// <summary>
+    /// Interaction logic for SignUpPage.xaml
+    /// </summary>
+    public partial class SignUpPage : Page
+    {
+        private SignUpVM vm;
+        public SignUpPage()
+        {
+            InitializeComponent();
+            vm = new SignUpVM();
+            DataContext = vm;
+        }
+
+        private void uname_TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (vm.Username == "Ex: abc2021")
+            {
+                uname_TextBox.Text = string.Empty;
+            }
+     
+            
+
+        }
+
+        private void pwd_TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (vm.Password == "Type your password here...")
+            {
+                pwd_TextBox.Text = string.Empty;
+            }
+        }
+
+        private void email_TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+            if (vm.Email == "Ex: yourname@gmail.com")
+            {
+                email_TextBox.Text = string.Empty;
+            }
+        }
+    }
+}
